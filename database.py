21612530511ad71082474d355cbaab241777cf71
Lim Qian Wen <limqianwen7@gmail.com>
2026-07-11 21:43:37 +0800

    Debug database connection

--- database.py
+++ database.py
@@ -6,6 +6,7 @@ def connect_database():
     try:
         connection = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
